96976fef3ea798ff8fd9b24df7e53d4174182915
Jeffrey Warren <jeff@unterbahn.com>
2019-03-29 15:47:02 -0400

    Update sketch.js

--- examples/fft/sketch.js
+++ examples/fft/sketch.js
@@ -3,42 +3,47 @@ var mic, fft,
   userInteracted = false; // need to wait for user interaction before using WebAudio https://developers.google.com/web/updates/2017/09/autoplay-policy-changes#webaudio
 
 function setupEqualizer() {
-  createCanvas(350, 100);
-  noFill();
-  $('.equalizer').append($('.p5Canvas').remove()) 
-  resizeCanvas(350, 100);
-  $('.eq-btn').hide() 
 
-  userInteracted = true;
+  var myp5 = new p5( function(sketch) {
 
-  mic = new p5.AudioIn();
-  mic.start();
-  fft = new p5.FFT();
-  fft.setInput(mic);
-}
+    var x = 100; 
+    var y = 100;
 
-function draw() {
-  if (userInteracted) {
-    var scale = 1;
-    background(200);
- 
-    text(parseInt(map(mouseX, 0, width, 20, 15000 / scale)) + " Hz", mouseX - 10, mouseY - 20);
-    var spectrum = fft.analyze();
- 
-    beginShape();
-    vertex(0, height * 0.9);
-    for (i = 0; i < spectrum.length / scale; i++) {
-      vertex(i * scale, map(spectrum[i], 0, 255, height * 0.8, 0));
-    }
-    vertex(width, height * 0.9);
-    endShape();
- 
-    var freq
-    // draw labels
-    for (i = 0; i < spectrum.length / scale; i += width / labelScale) {
-      freq = parseInt(map(i, 0, 1024, 20, 15000));
-      text(freq, map(i, 0, 1024 / scale, 0, width), height * 0.97);
-      fill(0, 100);
-    }
-  }
+    sketch.setup = function() {
+      sketch.createCanvas(350, 100);
+      sketch.noFill();
+
+      userInteracted = true;
+
+      mic = new p5.AudioIn();
+      mic.start();
+      fft = new p5.FFT();
+      fft.setInput(mic);
+    };
+
+    sketch.draw = function() {
+      if (userInteracted) {
+        var scale = 1;
+        sketch.background(200);
+
+        sketch.text(parseInt(map(sketch.mouseX, 0, sketch.width, 20, 15000 / scale)) + " Hz", sketch.mouseX - 10, sketch.mouseY - 20);
+        var spectrum = fft.analyze();
+
+        sketch.beginShape();
+        sketch.vertex(0, sketch.height * 0.9);
+        for (i = 0; i < spectrum.length / scale; i++) {
+          sketch.vertex(i * scale, map(spectrum[i], 0, 255, sketch.height * 0.8, 0));
+        }
+        sketch.vertex(sketch.width, sketch.height * 0.9);
+        sketch.endShape();
+
+        var freq
+        // draw labels
+        for (i = 0; i < spectrum.length / scale; i += sketch.width / labelScale) {
+          freq = parseInt(map(i, 0, 1024, 20, 15000));
+          sketch.text(freq, map(i, 0, 1024 / scale, 0, sketch.width), sketch.height * 0.97);
+          sketch.fill(0, 100);
+        }
+    };
+  }, $('.equalizer')[0]);
 }
